941fdf78018c53f2b458ff769289944e039fd90b
Nikandr Marhal <n.marhal@student.rug.nl>
2025-10-18 14:55:46 +0200

    refactor

--- algorithms/character_analysis.py
+++ algorithms/character_analysis.py
@@ -3,8 +3,11 @@ from model.book_names import BOOK_NAMES
 from model.episode_names import EPISODE_NAMES
 from model.read_data import *
 
+def _get_characters_sorted_by_centrality_scores(centrality_dict: dict):
+    return sorted(centrality_dict.items(), key=lambda x: x[1], reverse=True)
+
 def _print_centrality_scores(centrality_dict: dict, title: str):
-    sorted_characters = sorted(centrality_dict.items(), key=lambda x: x[1], reverse=True)
+    sorted_characters = _get_characters_sorted_by_centrality_scores(centrality_dict)
     print(f"\n{title}")
     for character, centrality in sorted_characters:
         print(f"  {character}: {centrality}")
@@ -18,8 +21,7 @@ def _get_heading(section_type: str, section_number: int):
         section_name = "Avatar: The Legend of Aang"
     return f"{section_type.upper()} {section_number}: {section_name}"
 
-def _print_analysis(section_type: str, section_number: int, data_frame: pd.DataFrame):
-    analyzer = NetworkStatisticsAnalyzer(data_frame)
+def _print_analysis(section_type: str, section_number: int, analyzer: NetworkStatisticsAnalyzer):
     centrality_measures = analyzer.get_centrality_scores()
 
     print(f"\n{'=' * 50}")
@@ -33,14 +35,17 @@ def _print_analysis(section_type: str, section_number: int, data_frame: pd.DataF
 
 def analyze_full_script():
     full_script_data = get_x_mentions_y()
-    _print_analysis(section_type="series", section_number=1, data_frame=full_script_data)
+    analyzer = NetworkStatisticsAnalyzer(full_script_data)
+    _print_analysis(section_type="series", section_number=1, analyzer=analyzer)
 
 def analyze_each_book():
     all_books = get_x_mentions_y_per_book()
     for book_number, book in enumerate(all_books, start=1):
-        _print_analysis(section_type="book", section_number=book_number, data_frame=book)
+        analyzer = NetworkStatisticsAnalyzer(book)
+        _print_analysis(section_type="book", section_number=book_number, analyzer=analyzer)
 
 def analyze_each_episode():
     all_episodes = get_x_mentions_y_per_episode()
     for episode_number, episode in enumerate(all_episodes, start=1):
-        _print_analysis(section_type="episode", section_number=episode_number, data_frame=episode)
+        analyzer = NetworkStatisticsAnalyzer(episode)
+        _print_analysis(section_type="episode", section_number=episode_number, analyzer=analyzer)
