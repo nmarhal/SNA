201d8e5d578e7649d4a20e91d9b356ad6a3fddce
Nikandr Marhal <n.marhal@student.rug.nl>
2025-10-25 16:58:55 +0200

    saving centralities to separate tables

--- algorithms/character_analysis.py
+++ algorithms/character_analysis.py
@@ -23,42 +23,55 @@ def _get_heading(section_type: str, section_number: int):
         return "Avatar: The Legend of Aang"
     return f"{section_type.upper()} {section_number}: {section_name}"
 
-def _get_combined_centralities(analyzer: NetworkStatisticsAnalyzer):
-    centralities = analyzer.get_centrality_scores()
-    in_degree = centralities.in_degree
-    eigenvector = centralities.eigenvector
-    betweenness = centralities.betweenness
-    characters = in_degree.keys()
-    combined_centralities = dict()
-    for character in characters:
-        character_in_degree = in_degree.get(character, 0)
-        character_eigenvector = eigenvector.get(character, 0)
-        character_betweenness = betweenness.get(character, 0)
-        combined_centralities[character] = (character_in_degree, character_eigenvector, character_betweenness)
-    return combined_centralities
+def _get_top_centrality(centrality_dict: dict, take_first: int = None):
+    if take_first is not None:
+        sorted_items = sorted(centrality_dict.items(), key=lambda x: x[1], reverse=True)
+        return dict(sorted_items[:take_first])
+    return centrality_dict
 
 def analyze_full_script_centralities():
     full_script_data = get_x_mentions_y()
     analyzer = NetworkStatisticsAnalyzer(full_script_data)
-    combined_centralities = _get_combined_centralities(analyzer)
+    centralities = analyzer.get_centrality_scores()
     heading = _get_heading(section_type="series", section_number=1)
-    save_centralities_to_csv(centralities=combined_centralities, heading=heading)
+
+    in_degree = _get_top_centrality(centralities.in_degree, take_first=20)
+    eigenvector = _get_top_centrality(centralities.eigenvector, take_first=20)
+    betweenness = _get_top_centrality(centralities.betweenness, take_first=20)
+
+    save_centrality_to_csv(in_degree, "in-degree", heading)
+    save_centrality_to_csv(eigenvector, "eigenvector", heading)
+    save_centrality_to_csv(betweenness, "betweenness", heading)
 
 def analyze_each_book_centralities():
     all_books = get_x_mentions_y_per_book()
     for book_number, book in enumerate(all_books, start=1):
         analyzer = NetworkStatisticsAnalyzer(book)
-        combined_centralities = _get_combined_centralities(analyzer)
+        centralities = analyzer.get_centrality_scores()
         heading = _get_heading(section_type="book", section_number=book_number)
-        save_centralities_to_csv(centralities=combined_centralities, heading=heading)
+
+        in_degree = _get_top_centrality(centralities.in_degree, take_first=20)
+        eigenvector = _get_top_centrality(centralities.eigenvector, take_first=20)
+        betweenness = _get_top_centrality(centralities.betweenness, take_first=20)
+
+        save_centrality_to_csv(in_degree, "in-degree", heading)
+        save_centrality_to_csv(eigenvector, "eigenvector", heading)
+        save_centrality_to_csv(betweenness, "betweenness", heading)
 
 def analyze_each_episode_centralities():
     all_episodes = get_x_mentions_y_per_episode()
     for episode_number, episode in enumerate(all_episodes, start=1):
         analyzer = NetworkStatisticsAnalyzer(episode)
-        combined_centralities = _get_combined_centralities(analyzer)
+        centralities = analyzer.get_centrality_scores()
         heading = _get_heading(section_type="episode", section_number=episode_number)
-        save_centralities_to_csv(centralities=combined_centralities, heading=heading)
+
+        in_degree = _get_top_centrality(centralities.in_degree, take_first=10)
+        eigenvector = _get_top_centrality(centralities.eigenvector, take_first=10)
+        betweenness = _get_top_centrality(centralities.betweenness, take_first=10)
+
+        save_centrality_to_csv(in_degree, "in-degree", heading)
+        save_centrality_to_csv(eigenvector, "eigenvector", heading)
+        save_centrality_to_csv(betweenness, "betweenness", heading)
 
 def analyze_clustering_full_script():
     full_script_data = get_x_mentions_y()
@@ -67,7 +80,7 @@ def analyze_clustering_full_script():
     average_clustering = analyzer.get_average_clustering()
     transitivity = analyzer.get_transitivity()
     heading = _get_heading(section_type="series", section_number=1)
-    save_clustering_coefficient_to_csv(clustering_coefficient, average_clustering, transitivity, heading)
+    save_clustering_coefficient_to_csv(clustering_coefficient, heading)
 
 def analyze_clustering_per_book():
     all_books = get_x_mentions_y_per_book()
@@ -77,7 +90,7 @@ def analyze_clustering_per_book():
         clustering_coefficient = analyzer.get_clustering_coefficient()
         average_clustering = analyzer.get_average_clustering()
         transitivity = analyzer.get_transitivity()
-        save_clustering_coefficient_to_csv(clustering_coefficient, average_clustering, transitivity, heading)
+        save_clustering_coefficient_to_csv(clustering_coefficient, heading)
 
 def analyze_clustering_per_episode():
     all_episodes = get_x_mentions_y_per_episode()
@@ -87,4 +100,4 @@ def analyze_clustering_per_episode():
         clustering_coefficient = analyzer.get_clustering_coefficient()
         average_clustering = analyzer.get_average_clustering()
         transitivity = analyzer.get_transitivity()
-        save_clustering_coefficient_to_csv(clustering_coefficient, average_clustering, transitivity, heading)
+        save_clustering_coefficient_to_csv(clustering_coefficient, heading)
--- view/create_tables.py
+++ view/create_tables.py
@@ -8,8 +8,6 @@ def _fix_heading_characters(heading):
 
 def save_clustering_coefficient_to_csv(
     clustering_coefficient: dict,
-    average_clustering: float,
-    transitivity: float,
     heading: str
 ):
     clustering_tables_dir = os.path.join(tables_dir, "clustering")
@@ -23,25 +21,24 @@ def save_clustering_coefficient_to_csv(
     with open(filepath, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(['name', 'coefficient'])
-        writer.writerow(['average clustering coefficient', average_clustering])
-        writer.writerow(['transitivity', transitivity])
         for character, coefficient in sorted_data:
             writer.writerow([character, coefficient])
 
-def save_centralities_to_csv(
-    centralities: dict[str, tuple[float, float, float]],
+def save_centrality_to_csv(
+    centrality_dict: dict,
+    centrality_name: str,
     heading: str
 ):
-    clustering_tables_dir = os.path.join(tables_dir, "centrality")
-    os.makedirs(clustering_tables_dir, exist_ok=True)
-
-    sorted_data = sorted(centralities.items(), key=lambda x: x[1], reverse=True)
-
+    centrality_dir = os.path.join(f"{tables_dir}/centralities", centrality_name)
+    os.makedirs(centrality_dir, exist_ok=True)
+    
+    sorted_data = sorted(centrality_dict.items(), key=lambda x: x[1], reverse=True)
+    
     filename = _fix_heading_characters(heading)
-    filepath = os.path.join(clustering_tables_dir, filename)
-
+    filepath = os.path.join(centrality_dir, filename)
+    
     with open(filepath, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(["name", "in-degree", "eigenvector", "betweenness"])
-        for character, (in_degree, eigenvector, betweenness) in sorted_data:
-            writer.writerow([character, in_degree, eigenvector, betweenness])
+        writer.writerow(["name", centrality_name])
+        for character, score in sorted_data:
+            writer.writerow([character, f"{score:.3f}"])
